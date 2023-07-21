54ffb5f70c494bb8367ae67d60deb010cb887ac2
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-07-21 11:26:25 -0300

    Add files via upload

---
+++ projeto_1_Spring/src/main/java/com/example/demo/Projeto1SpringApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1SpringApplication.class, args);
+		
+		
+		
+	}
+
+}
